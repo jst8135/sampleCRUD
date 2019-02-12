b23feaeca2c42676fdbbee4ba9211b893362bb41
Administrator <Administrator@smart20105>
2019-02-12 17:08:39 +0900

    [주선태]SampleMapper,SampleService생성

--- web02/src/main/java/com/eteam/web02/controller/SampleController.java
+++ web02/src/main/java/com/eteam/web02/controller/SampleController.java
@@ -7,17 +7,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.PostMapping; 
 import org.springframework.web.bind.annotation.RequestParam; 
- 
+
  
 import com.eteam.web02.service.SampleService; 
  
  
 @Controller 
 public class SampleController { 
- 	@Autowired  
- 	private SampleService sampleService; 
- 	// 1. 입력폼 
- 	@GetMapping("/addSample") 
+	@Autowired  
+	private SampleService sampleService; 
+	// 1. 입력폼 
+	@GetMapping("/addSample") 
  	public String addSample() { 
  		return "addSample";	//view 이름은 template폴더의 addSample.html, 포워드 
  	} 
@@ -36,4 +36,4 @@ public class SampleController {
  	// 5. 수정 폼 
  	 
  	// 6. 수정 액션 
- } 
+} 
--- web02/src/main/java/com/eteam/web02/service/SampleService.java
+++ web02/src/main/java/com/eteam/web02/service/SampleService.java
@@ -2,11 +2,11 @@ package com.eteam.web02.service;
  
  
 import java.util.List; 
- 
+
  
 import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Service; 
-
+ 
  
 import com.eteam.web02.mapper.SampleMapper; 
 import com.eteam.web02.vo.Sample; 
@@ -14,26 +14,26 @@ import com.eteam.web02.vo.Sample;
  
 @Service 
 public class SampleService { 
-	@Autowired  
-	private SampleMapper sampleMapper; 
-	 
-	public List<Sample> getSampleList() { 
-		return null; 
-	} 
-	 
+ 	@Autowired  
+ 	private SampleMapper sampleMapper; 
+ 	 
+ 	public List<Sample> getSampleList() { 
+ 		return null; 
+ 	} 
+ 	 
  	public List<Sample> getSampleOne() { 
-		return null; 
-	} 
-	 
-	public int addSample(Sample sample) { 
-		return 0; 
-	} 
-	 
-	public int removeSample(Sample sample) { 
-		return 0; 
-	} 
-	 
-	public int modifySample(Sample sample) { 
-		return 0; 
-	} 
+ 		return null; 
+ 	} 
+ 	 
+ 	public int addSample(Sample sample) { 
+ 		return 0; 
+ 	} 
+ 	 
+ 	public int removeSample(Sample sample) { 
+ 		return 0; 
+ 	} 
+ 	 
+ 	public int modifySample(Sample sample) { 
+ 		return 0; 
+ 	} 
 } 
